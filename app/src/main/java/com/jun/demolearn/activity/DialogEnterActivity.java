59c7c18e7989332edd4fea1680df6c7be3359c51
吴俊 <wujun@wanwaytech.com>
2020-03-02 18:47:54 +0800

    增加一些对话框

--- app/src/main/java/com/jun/demolearn/activity/DialogEnterActivity.java
+++ app/src/main/java/com/jun/demolearn/activity/DialogEnterActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 
-import androidx.appcompat.app.AppCompatActivity;
 import com.jun.demolearn.R;
 import com.jun.demolearn.base.BaseActivity;
 import com.jun.demolearn.dialog.NormalConfirmActivityDialog;
