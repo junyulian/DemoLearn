f5e7796a7de5bfbe3aa39284c758d5a2915716f0
吴俊 <wujun@wanwaytech.com>
2020-03-02 19:16:56 +0800

    增加一些工具类

--- app/src/main/java/com/jun/demolearn/App.java
+++ app/src/main/java/com/jun/demolearn/App.java
@@ -1,6 +1,7 @@
 package com.jun.demolearn;
 
 import android.app.Application;
+import android.os.Handler;
 
 import com.jun.utils.common.AppConfig;
 
@@ -9,12 +10,33 @@ import com.jun.utils.common.AppConfig;
 public class App extends Application {
 
     private static App app;
+    private static int mainThreadId;//主线程id
+    private static Handler mHandler;//处理需运行在主线程任务
 
     @Override
     public void onCreate() {
         super.onCreate();
         app = this;
+        mainThreadId = android.os.Process.myPid();//主线程id
+        mHandler = new Handler();//运行在主线程
         AppConfig.getInstance().init(app);//初始化app的全局配置
 
     }
+
+    /**
+     * 主线程id
+     * @return
+     */
+    public static int getMainThreadId(){
+        return mainThreadId;
+    }
+
+    /**
+     * 此handler处理需运行在主线程的任务
+     * @return
+     */
+    public static Handler getHandler(){
+        return mHandler;
+    }
+
 }
---
+++ app/src/main/java/com/jun/demolearn/utils/ThreadUtil.java
@@ -0,0 +1,46 @@
+package com.jun.demolearn.utils;
+
+import android.os.Handler;
+
+import com.jun.demolearn.App;
+
+
+public class ThreadUtil {
+
+    /**
+     * 主线程id
+     * @return
+     */
+    public static int getMainThreadId(){
+        return App.getMainThreadId();
+    }
+
+    /**
+     * 此handler处理需运行在主线程的任务
+     * @return
+     */
+    public static Handler getHandler(){
+        return App.getHandler();
+    }
+
+    /**
+     * 是否在主线程运行
+     * @return
+     */
+    public static boolean isRunOnUIThread(){
+        int myTid = android.os.Process.myTid();
+        if(myTid == getMainThreadId()){
+            return true;
+        }
+        return false;
+    }
+
+    public static void runOnUiThread(Runnable runnable){
+        if(isRunOnUIThread()){
+            runnable.run();
+        }else {
+            getHandler().post(runnable);
+        }
+    }
+
+}
